2877a7e82ab758e7471aae66696cec90298f2107
alexhkurz <alexhkurz@gmail.com>
2023-07-23 21:16:58 +0200

    clean up for voting project

--- frontend/routes/_app.tsx
+++
@@ -1,9 +0,0 @@
-import LayoutWrapper from '~/components/LayoutWrapper.tsx';
-
-export default function App({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <LayoutWrapper>{children}</LayoutWrapper>
-    </>
-  );
-}
--- lectures/README.md
+++
@@ -1,5 +0,0 @@
-# README
-
-This folder contains additional material we used in the lectures.
-
-For a timeline see [lecture-by-lecture](lecture-by-lecture.md).
