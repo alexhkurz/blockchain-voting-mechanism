f4a05146523787c54fe40e78c76be01954388c09
WunderBear <47909923+finncat62@users.noreply.github.com>
2023-10-19 21:22:34 -0700

    got rid of token, own implentation works Perfect

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -1,80 +1,34 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
-// 1st import as follows
-import "contracts/Imports.sol";
-
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
 contract Proposal {
-    address creator;
-    string title;
-    string description;
-    address topicAddress;
-    TopicToken token;
+    // If you can limit the length to a certain number of bytes, 
+    // always use one of bytes1 to bytes32 because they are much cheaper
+    string title;   // short name (up to 32 bytes)
+    constructor(string memory _title) {title=_title;}
+    uint public voteCount; // number of accumulated votes
+    mapping(address => uint) voterBalances;
 
-    uint goal;//used if the solution wants people to send in money
-    mapping(address => uint) public userBalance;
+    function getVoterBalance(address sender)public view returns(uint voterBalance){
+ 
+        return voterBalances[sender];
     
-    event transferRecieved(address from, uint amount); 
-    constructor(string memory _proposalTitle, string memory _proposalDescription, address _topicAddress, TopicToken _token) payable{
-        creator = msg.sender;
-        title = _proposalTitle;
-        description = _proposalDescription;
-        topicAddress = _topicAddress;
-        token = _token;
     }
-    
-// recieve is not being called on transaction to this proposal
-    receive() payable external { // recieve votes from user
-       //does not check it is the right token yet
-       emit transferRecieved(msg.sender, msg.value);
-        userBalance[msg.sender] += msg.value;
-        console.log("recieved token: ");
-        console.log(msg.value);
+    function getTotalVotes()public view returns(uint voteCount_){
+        return voteCount;
+   
     }
 
-    modifier onlyOwner(){ // only owner can access function
-        require(creator == msg.sender, "not owner");
-        _;
+    function withdrawVote(address sender, uint _amount) public {
+        voterBalances[sender] -= _amount;
+        voteCount -=_amount;
     }
 
-    function getTotalBalence() public view returns(uint){// returns total balance
-        return address(this).balance;
+    function giveVote(address sender,uint _amount) payable  public {
+        voterBalances[sender] += _amount;// this is the line it is breaking
+        voteCount+=_amount;
     }
 
-    function getUserBalence() public view returns(uint){// returns user balance
-        return userBalance[msg.sender];
-    }
-    
-    function withdraw(address _sender, uint _amount) external payable returns(uint){// returns user balance
-        //does not yet check request is through parent topic
-        //require(userBalance[msg.sender] >= _amount, "can't withdraw more then you have");
-       
-        userBalance[msg.sender] -= _amount;
-        token.approve(address(this), _amount);
-        token.transferFrom(address(this),payable(_sender), _amount);
-        return userBalance[msg.sender];
-    }
-
-    function setDescription(string memory _description) onlyOwner public {
-        require (msg.sender == creator, "must be creator to set description");
-        description = _description;
-    }
-
-    function getDescription() public view returns (string memory){//returns description
-        return description;
-    }
-
-    function setName(string memory _title) onlyOwner public {
-        require (msg.sender == creator, "must be creator to set description");
-        title = _title;
-    }
-
-    function getName() view public returns (string memory){// returns name
+    function getTitle() public view returns(string memory title_){
         return title;
     }
-
-    function getThisAddress()public view returns(address){//returns this contract address
-        return address(this);
-    }
-
 }
--- contracts/Topic.sol
+++ contracts/Topic.sol
@@ -1,193 +1,91 @@
-//we change allowance and transfer things back and forth with different values to see if we can track what is happening.
-//dispite everything, we are unable to make any changes to the users allowance.
-//this inability to increase userr allowance means that the user can not transfer to anything
-//however, we can easily change the topic's allowance. A work around may be to always have the 
-//topic transfer tokens, keeping track of each user. The user wouldn't actually have the tokens at all, just telling the topic when to transfer.
+// SPDX-License-Identifier: GPL-3.0
 
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity >=0.7.0 <0.9.0;
 
 import "contracts/Proposal.sol";
-import "contracts/Imports.sol";
 
 contract Topic {
+    
+    struct Voter {
+        uint balance;   // how many votes they have left
+        Proposal[] votedFor; //array of voted for proposals
+        bool hasRequestedVote;//if hasn't requested vote, allows balance to become the standard alloted balance
+    }
+
+    mapping(address => Voter) public voters;
    
-    string title;
-    string description;
-    address topicAddress;
-
-    TopicToken token;
+    string[] proposalTitles;
+    mapping (string => address) public proposalAddressFromTitle;
+    mapping(address => Proposal) public proposalFromAddress;
     
-    mapping(address => Proposal) proposals;
-    address[] listOfProposalAddresses;
-    address[] listOfVoters;
+    //todo emit events
+    constructor() {}
 
-    enum conclude_vote_types{time,threshold}
-    enum voting_mode_types{firstPastThePost,runoff_twoRound, rankedChoice,distruibuted,distributed_ranked_choice}
-    conclude_vote_types concludeVoteMode = conclude_vote_types.time;
-    voting_mode_types votingMode = voting_mode_types.distributed_ranked_choice;
-    
-    event transferSent(address _from, address _destAddr, uint _amount); 
-    event withdrawFromProposalOnTopicSide(address person, address fromProposal, uint amount);
-   
-    constructor(string memory _topicTitle, string memory _topicDescription) {
-        title = _topicTitle;
-        description = _topicDescription;
-        topicAddress = address(this);
-        token = new TopicToken("Topic_Token", "TTK");
+    function getUserBalance() public view returns(uint balance){
+        return voters[msg.sender].balance;
     }
-    
-    //proposal stuff
-    function makeNewProposal(string memory _name,string memory _description)public returns (address){
-        Proposal newProposal = new Proposal(_name,_description,topicAddress, token);
-        proposals[address(newProposal)] = newProposal;
-        listOfProposalAddresses.push(address(newProposal));
-        return address(newProposal);
+    function makeNewProposal(string memory _title) public {
+        Proposal newProposal = new Proposal(_title);
+        proposalAddressFromTitle[_title] = address(newProposal);
+        proposalFromAddress[address(newProposal)] = newProposal;
+        proposalTitles.push(_title);
     }
 
-    function getallProposals() public view returns(address[] memory){
-        return listOfProposalAddresses;
+    function requestVote() public {
+        if(! voters[msg.sender].hasRequestedVote){
+            voters[msg.sender].balance = 50;
+            voters[msg.sender].hasRequestedVote = true;
+        }
     }
 
-    function getProposalDescription(address _proposalAddr) public view returns(string memory){
-       return proposals[_proposalAddr].getDescription();
-    }
-    function getProposalName(address _proposalAddr) public view returns(string memory){
-       return proposals[_proposalAddr].getName();
-    }
-
-
-    //proposal transaction stuff
-    function getUserBalance() public view returns(uint){
-        return token.balanceOf(msg.sender);
-    }
-    function getTotalSupply() public view returns(uint){
-        return token.totalSupply();
-    }
-    function withdrawFromProposal(address _proposalAddr,uint _amount) proposalExists(_proposalAddr) public {
-        
-        //require(proposals[_proposalAddr].getUserBalence() > 0, "cant take nothing");
-        token.approve(_proposalAddr, _amount);
-        token.approve(msg.sender, _amount);
-        proposals[_proposalAddr].withdraw(msg.sender,_amount);
-        
-        //token.decreaseAllowance(_proposalAddr, _amount);
-        //token.increaseAllowance(payable(msg.sender), _amount);
-        emit withdrawFromProposalOnTopicSide(msg.sender,_proposalAddr,_amount);
+    function vote(string memory _proposalTitle,uint _amount) public checkProposalExists(_proposalTitle)  {
+        require (voters[msg.sender].balance >= _amount, "have to have enough votes to send");
+        proposalFromAddress[proposalAddressFromTitle[_proposalTitle]].giveVote(msg.sender,_amount);     
+        voters[msg.sender].balance -= _amount;
     }
 
-    function sendToProposal(address _proposalAddr,uint _amount) proposalExists(_proposalAddr) public {
-        require(token.balanceOf(msg.sender) >= _amount, "cant give more than ya have");
-        address senderAddress = address(msg.sender);
-        //token.approve(msg.sender, _amount);
-        
-        //token.increaseAllowance(_proposalAddr,_amount);//get rid of one of these, approve isn't working as it should
-        //token.decreaseAllowance(payable(msg.sender), 5);
-        //token.Transfer(from, to, value);
-        
-        //token.transferFrom(msg.sender, _proposalAddr, _amount);
-        console.log("topic allowance");
-        console.log(token.allowance(address(this), address(this)));
-        
-        console.log("topic balance");
-        console.log(token.balanceOf(address(this)));
-        
-        console.log("user allowance");
-        console.log(token.allowance(senderAddress,senderAddress));
-        
-        console.log("user balance");
-        console.log(token.balanceOf(senderAddress));
-
-        console.log("proposal allowance");
-        console.log(token.allowance(_proposalAddr,_proposalAddr));
-        
-        console.log("proposal balance");
-        console.log(token.balanceOf(_proposalAddr));
-               
-
-        token.approve(senderAddress, 1 * 10**uint(token.decimals()));
-
-        token.approve(topicAddress,2 * 10**uint(token.decimals()));
-        token.approve(address(this),3 * 10**uint(token.decimals()));
-        token.increaseAllowance(senderAddress, 1000);
-        token.increaseAllowance(address(this), 2000);
-        
-        token.approve(senderAddress, 999999);
-        //token.transferFrom(msg.sender, _proposalAddr, 5);
-     //   console.log("usr address: ");
-     //   console.log(msg.sender);
-
-        console.log("AFTER TRANSFER: ----------------------------------------------------------");
-        console.log("topic allowance");
-        console.log(token.allowance(address(this), address(this)));
-        
-        console.log("topic balance");
-        console.log(token.balanceOf(address(this)));
-        
-        console.log("user allowance");
-        console.log(token.allowance(senderAddress,senderAddress));
-        
-        console.log("user balance");
-        console.log(token.balanceOf(senderAddress));
-
-        console.log("proposal allowance");
-        console.log(token.allowance(_proposalAddr,_proposalAddr));
-        
-        console.log("proposal balance");
-        console.log(token.balanceOf(_proposalAddr));
-        emit transferSent(msg.sender, _proposalAddr, _amount);
-        
-        
+    function withdrawVote(string memory _proposalTitle,uint _amount) public{
+        require(proposalFromAddress[proposalAddressFromTitle[_proposalTitle]].getVoterBalance(msg.sender) >= _amount, "can't withdraw what you haven't put in");
+        proposalFromAddress[proposalAddressFromTitle[_proposalTitle]].withdrawVote(msg.sender, _amount);
+        voters[msg.sender].balance += _amount;
     }
 
-    function requestToken() isNewVoter public {
-        listOfVoters.push(msg.sender);
-       
-        
-       //token.mint(msg.sender,5);
-        token.mint(address(this), 100 * 10**uint(token.decimals()));//;
-        token.approve(msg.sender, 5 * 10**uint(token.decimals()));
-
-        token.increaseAllowance(msg.sender, 200);
-        token.increaseAllowance(address(this),200);
-
-        token.approve(topicAddress,6 * 10**uint(token.decimals()));
-        token.approve(address(this),7 * 10**uint(token.decimals()));
-        token.transferFrom(address(this),msg.sender, 4 * 10**uint(token.decimals()));
-        //token.transfer(msg.sender, 5 * 10**uint(token.decimals()));
-        
-        
-    }
-
-
-    function getTokenAddress() public view  returns (TopicToken){
-        return token;
-    }
-    modifier proposalExists(address _proposalAddr){
-        bool propExists = false;
-        for(uint i = 0; i < listOfProposalAddresses.length; i++){
-            if(_proposalAddr == listOfProposalAddresses[i]){
-                propExists = true;
-                break;
+    modifier checkProposalExists(string memory _proposalTitle) {
+        bool proposalExists = false;
+        for(uint i = 0; i <= proposalTitles.length;i++){
+            if( stringsEqual(_proposalTitle,proposalFromAddress[proposalAddressFromTitle[_proposalTitle]].getTitle())){
+                proposalExists = true;
             }
         }
-        require(propExists == true, "proposal address not valid");
-        _;
-
+        require(proposalExists, "proposal does not exist");
+            _;
     }
-    modifier isNewVoter {
-        bool canVote = true;
 
-        for(uint i =0; i < (listOfVoters.length);i++){
-            if(msg.sender == listOfVoters[i]){
-                canVote = false;
+    function stringsEqual(string memory s1, string memory s2) private pure returns (bool) {
+        bytes memory b1 = bytes(s1);
+        bytes memory b2 = bytes(s2);
+        uint256 l1 = b1.length;
+        if (l1 != b2.length) return false;
+        for (uint256 i=0; i<l1; i++) {
+            if (b1[i] != b2[i]) return false;
+
+        }
+        return true;
+    }
+
+    function getWinningProposalName() public view returns (string memory winningProposalName){
+        uint winningVoteCount = 0;
+
+        for (uint p = 0; p < proposalTitles.length; p++) {
+            if (proposalFromAddress[proposalAddressFromTitle[proposalTitles[p]]].getTotalVotes() > winningVoteCount) {
+                winningVoteCount = proposalFromAddress[proposalAddressFromTitle[proposalTitles[p]]].getTotalVotes();
+                winningProposalName = proposalTitles[p];
             }
         }
-        
-        require(canVote == true,"not a new voter");
-        _;
+        return winningProposalName;
     }
-    
+
+    function winningProposalAddress() public view returns (address winnerAddress_){
+        return proposalAddressFromTitle[getWinningProposalName()];
+    }
 }
-
